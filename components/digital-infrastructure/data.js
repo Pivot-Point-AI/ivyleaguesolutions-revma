b93ae58c65c42eb152339d376d02009ae7ae97bb
zuhooruddin <zuhooruddin055@gmail.com>
2026-07-24 10:54:35 +0500

    resolved

--- components/digital-infrastructure/data.js
+++ components/digital-infrastructure/data.js
@@ -1,16 +1,16 @@
 export const cards = [
   {
-    icon: "databackupLogo.svg",
+    icon: "databackupLogo.webp",
     heading: "Data Backup and Recovery",
     body: "Protect your critical data with our reliable backup solutions, ensuring your information is secure and recoverable in emergencies.",
   },
   {
-    icon: "disasterLogo.svg",
+    icon: "disasterLogo.webp",
     heading: "Disaster Recovery as a Service",
     body: "Our Data Recovery solutions ensure business continuity by providing forceful recovery plans that reduce interruption during unexpected incidents.",
   },
   {
-    icon: "cloudsecurityLogo.svg",
+    icon: "cloudsecurityLogo.webp",
     heading: "Cloud Security Services",
     body: "Protect your cloud infrastructure with advanced security measures adapted to your specific requirements, protecting your data from threats.",
   },
